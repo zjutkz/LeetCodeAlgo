ea25875288bf51edb79fb17bc4b51c1684c207da
亢哲 <kangzhe@weidian.com>
2019-06-03 18:15:21 +0800

    median

---
+++ algo/src/com/zjutkz/leetcode/median/Test.java
@@ -0,0 +1,13 @@
+package com.zjutkz.leetcode.median;
+
+/**
+ * Created by kangzhe on 19/6/3.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 3};
+        int[] nums2 = new int[]{2, 5};
+        System.out.println(new Solution().findMedianSortedArrays(nums1, nums2));
+    }
+}
